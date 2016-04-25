26885d4f0509e6193a0cc533ce43cab5c223ff54
andy-dufour <adufour@chef.io>
2016-04-25 11:43:00 -0400

    Fixing rubocop offenses

--- libraries/interval.rb
+++ libraries/interval.rb
@@ -2,11 +2,8 @@
 
 def last_run(profile, interval)
   # Calculate when the profile was last run so we delay it's next run if necessary
+  return false unless ::File.exist?("#{compliance_cache_directory}/#{profile}")
   compliance_cache_directory = ::File.join(Chef::Config[:file_cache_path], 'compliance')
-  if ::File.exist?("#{compliance_cache_directory}/#{profile}")
-    lastrun = Time.now - ::File.mtime("#{compliance_cache_directory}/#{profile}")
-    return lastrun < interval
-  else
-    return false
-  end
+  lastrun = Time.now - ::File.mtime("#{compliance_cache_directory}/#{profile}")
+  lastrun < interval
 end
