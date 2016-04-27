154beed131508b12152fb465e83ff4a758f602be
Alex Pop <al3xpop@gmail.com>
2016-04-27 19:08:41 +0100

    add profile name validation
    and chefspec tests to cover runlist and success/failure

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -22,12 +22,59 @@ require 'spec_helper'
 describe 'audit::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+  end
+
+
+  context 'When two profiles are specified' do
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '6.5')
+      runner.node.set['audit']['profiles'] = { "admin/myprofile" => true,
+                                               "base/ssh" => false }
+      runner.converge(described_recipe)
+    end
+
+    it 'runs the compliance_profile' do
+      expect(chef_run).to fetch_compliance_profile('myprofile').with(
+          owner: 'admin',
+          server: nil,
+          token: nil,
+          inspec_version: 'latest'
+        )
+      expect(chef_run).to execute_compliance_profile('myprofile').with(
+          owner: 'admin',
+          server: nil,
+          token: nil,
+          inspec_version: 'latest'
+        )
+      expect(chef_run).to execute_compliance_report('chef-server').with(
+          owner: nil,
+          server: nil,
+          token: nil,
+          variant: 'chef'
+        )
+    end
+
+    it 'converges successfully' do
+      expect{chef_run}.to_not raise_error
+    end
+  end
+
+  context 'When invalid profile is passed' do
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '6.5')
+      runner.node.set['audit']['profiles'] = { "myprofile" => true }
+      runner.converge(described_recipe)
+    end
+
+    it 'does raise an error' do
+      expect{chef_run}.to raise_error("Invalid profile name 'myprofile'. Must contain /, e.g. 'john/ssh'")
     end
   end
 end
