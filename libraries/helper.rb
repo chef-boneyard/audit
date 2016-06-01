de81f6f97b891ae3f0cd89c900ff53347d0fb0a9
Christoph Hartmann <chris@lollyrock.com>
2016-06-01 23:13:05 +0200

    improve handling of auth errors

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -18,21 +18,29 @@ module ComplianceHelpers
     server
   end
 
+  def handle_http_error_code(code)
+    case code
+    when /401/
+      Chef::Log.error 'Possible time/date issue on the client.'
+    when /403/
+      Chef::Log.error 'Possible offline Compliance Server or chef_gate auth issue.'
+    when /404/
+      Chef::Log.error 'Object does not exist on remote server.'
+    end
+    msg = 'Could not fetch the profile. Verify the authentication (e.g. token) is set properly'
+    Chef::Log.error msg
+    fail msg if run_context.node.audit.raise_if_unreachable
+  end
+
   #rubocop:disable all
   def with_http_rescue(&block)
     begin
-      return yield
+      response = yield
+      # handle non 200 error codes, they are not raised as Net::HTTPServerException
+      handle_http_error_code(response.code) if response.code.to_i >= 300
+      return response
     rescue Net::HTTPServerException => e
-      case e.response.code
-      when /401/
-        Chef::Log.error "Possible time/date issue on the client."
-      when /403/
-        Chef::Log.error "Possible offline Compliance Server or chef_gate auth issue."
-      when /404/
-        Chef::Log.error "Object does not exist on remote server."
-      end
-      Chef::Log.error e.message
-      raise e if run_context.node.audit.raise_if_unreachable
+      handle_http_error_code(e.response.code)
     end
   end
 
