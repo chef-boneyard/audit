42a9d515ee059e15f313528d9f27bb9497b101b7
Jeremy J. Miller <jm@chef.io>
2016-09-13 22:06:32 -0400

    profile upload capability

--- libraries/profile.rb
+++ libraries/profile.rb
@@ -21,6 +21,7 @@ class Audit
       property :insecure, [TrueClass, FalseClass], default: false
       property :formatter, ['json', 'json-min'], default: 'json-min'
       property :quiet, [TrueClass, FalseClass], default: true
+      property :overwrite, [TrueClass, FalseClass], default: true
       # TODO(sr) it might be nice to default to settings from attributes
 
       # alternative to (owner, profile)-addressing for profiles,
@@ -136,6 +137,46 @@ class Audit
             content runner.report.to_json
             sensitive true
             backup false
+          end
+        end
+      end
+
+      action :upload do
+        converge_by 'profile validation checks' do
+          raise 'Path to profile archive not specified' if path.nil?
+          raise "Profile archive file #{path} does not exist." unless ::File.exist?(path)
+          profile = Inspec::Profile.for_target(path, {})
+          result = profile.check
+          unless result[:summary][:valid]
+            raise 'Profile check failed. Please fix the profile before upload.'
+          else
+            Chef::Log.info 'Profile is valid'
+          end
+        end
+
+        converge_by 'upload compliance profile' do
+          o, p = normalize_owner_profile
+          node.run_state['compliance'] ||= {}
+
+          if node.run_state['compliance']['access_token']
+            reqpath ="owners/#{o}/compliance/#{p}/tar"
+            url = construct_url(server, reqpath)
+            Chef::Log.info "Upload compliance profile #{o}/#{p} to: #{url}"
+            config = Compliance::Configuration.new
+            config['token'] = node.run_state['compliance']['access_token']
+            config['insecure'] = insecure
+            config['server'] = server
+            config['version'] = Compliance::API.version(server, insecure)
+            if Compliance::API.exist?(config, "#{o}/#{p}") && !overwrite
+              raise 'Profile exists on the server, use property `overwrite`. Skipping upload!'
+            else
+              success, msg = Compliance::API.upload(config, o, p, path)
+              if success
+                Chef::Log.info 'Successfully uploaded profile'
+              else
+                Chef::Log.error "Error during profile upload: #{msg}"
+              end
+            end
           end
         end
       end
