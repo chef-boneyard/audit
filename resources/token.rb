a313068aebaf29fd8b4a0d57a5a117e89ed8a29b
Lamont Granquist <lamont@scriptkiddie.org>
2016-10-13 16:44:07 -0700

    convert library resources to proper custom resources
    library cookbooks are not a recommended cookbook pattern
    
    see:
    
    https://docs.chef.io/custom_resources_notes.html
    
    and issues like this which are directly related to trying to
    fight the resources DSL:
    
    https://github.com/chef-cookbooks/httpd/issues/97
    
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

---
+++ resources/token.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+# `compliance_token` custom resource to communicate with Chef Compliance
+include ComplianceHelpers
+
+provides :compliance_token
+
+property :server, [String, URI, nil], required: true
+property :port, Integer
+property :token, [String, nil], required: true
+property :insecure, [TrueClass, FalseClass], default: false
+
+default_action :create
+
+action :create do
+  converge_by 'compliance server auth token setup' do
+    # stash token in node.run_state to pass between resources
+    node.run_state['compliance'] ||= {}
+    if node['audit']['refresh_token']
+      Chef::Log.info 'Using refresh_token to exchange for an access token.'
+      node.run_state['compliance']['access_token'] = retrieve_access_token(server, token, insecure)
+    else
+      Chef::Log.info 'Using token attribute. This token is short lived and will expire.'
+      node.run_state['compliance']['access_token'] = token
+    end
+  end
+end
