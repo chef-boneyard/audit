43845a15bd1013e0308a0fc6f84c4d7bca41da8a
Victoria Jeffrey <vjeffrey@chef.io>
2016-10-17 09:25:51 -0400

    use chef handler to run inspec tests

---
+++ files/default/audit_report.rb
@@ -0,0 +1,39 @@
+require 'chef/handler'
+
+class Chef
+  class Handler
+    # Creates a compliance audit report
+    class AuditReport < ::Chef::Handler
+      def report
+        load_needed_dependencies
+        call
+      end
+
+      def load_needed_dependencies
+        require "inspec"
+        # load supermarket plugin, this is part of the inspec gem
+        require "bundles/inspec-supermarket/api"
+        require "bundles/inspec-supermarket/target"
+
+        # load the compliance plugin
+        require "bundles/inspec-compliance/configuration"
+        require "bundles/inspec-compliance/support"
+        require "bundles/inspec-compliance/http"
+        require "bundles/inspec-compliance/api"
+        require "bundles/inspec-compliance/target"
+      end
+
+      def call
+        Chef::Log.debug "Initialize InSpec"
+        opts = { "format" => node['audit']['format'] }
+        runner = ::Inspec::Runner.new(opts)
+
+        tests = node['audit']['profiles']
+        tests.each { |target| runner.add_target(target, opts) }
+
+        Chef::Log.debug "Running tests from: #{tests.inspect}"
+        exit_code = runner.run
+      end
+    end
+  end
+end
