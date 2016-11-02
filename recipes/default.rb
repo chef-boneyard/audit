b6b50fa3ac714b61a58076296b606dd2e45e3d52
Jeremy J. Miller <jm@chef.io>
2016-11-02 18:49:29 -0400

    fixing so report handler and inspec are not reported as updated resources
    Signed-off-by: Jeremy J. Miller <jm@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,22 +1,8 @@
-include_recipe 'chef_handler'
-
-# install inspec
-inspec 'inspec' do
+chef_gem 'inspec' do
   version node['audit']['inspec_version']
+  compile_time true
   action :install
 end
 
-# run chef handler
-handler_directory = ::File.join(Chef::Config[:file_cache_path], 'handler')
-directory handler_directory do
-  action :create
-end
-
-cookbook_file ::File.join(handler_directory, 'audit_report.rb') do
-  source 'audit_report.rb'
-end
-
-chef_handler 'Chef::Handler::AuditReport' do
-  source "#{handler_directory}/audit_report.rb"
-  action :enable
-end
+# load the audit report handler
+load_audit_handler
