d6f70ab20878b997087d2af06c30e5b81975d9f4
Jeremy J. Miller <jm@chef.io>
2016-11-02 19:25:09 -0400

    re-add support for 'latest' inspec gem version
    Signed-off-by: Jeremy J. Miller <jm@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 chef_gem 'inspec' do
-  version node['audit']['inspec_version']
+  version node['audit']['inspec_version'] if node['audit']['inspec_version'] != 'latest'
   compile_time true
   action :install
 end
