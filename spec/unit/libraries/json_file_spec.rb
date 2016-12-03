79421bd8a7d9f3d3fe9f9d90f2bee4ce1dc92e92
Jeremy J. Miller <jm@chef.io>
2016-12-02 23:07:09 -0500

    change json-min filename
    Signed-off-by: Jeremy J. Miller <jm@chef.io>

--- spec/unit/libraries/json_file_spec.rb
+++ spec/unit/libraries/json_file_spec.rb
@@ -9,7 +9,7 @@ require_relative '../../../libraries/collector_classes'
 describe 'Collector::JsonFile methods' do
   it 'writes the report to a file on disk' do
     report = 'some info'
-    timestamp = Time.now.utc.to_s.tr(' ', '_')
+    timestamp = Time.now.utc.strftime('%Y%m%d%H%M%S')
     @jsonfile = Collector::JsonFile.new(report, timestamp).send_report
     expected_file_path = File.expand_path("../../../../inspec-#{timestamp}.json", __FILE__)
     expect(File).to exist("#{expected_file_path}")
