10084d849dedf91cb99434e0221ff3c841bad693
Christoph Hartmann <chris@lollyrock.com>
2017-04-03 13:01:50 +0200

    use new gem option to test chef gems
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

--- test/integration/gem-install-version/default_spec.rb
+++ test/integration/gem-install-version/default_spec.rb
@@ -1,6 +1,5 @@
-#inspec (1.16.1)
-
-# verify that only inspec v1.16.1 is installed
-describe command('/opt/chef/embedded/bin/gem list --local -a -q inspec | grep \'^inspec\' | awk -F"[()]" \'{printf $2}\'') do
-  its('stdout') { should cmp '1.18.0' }
+# verify that a specific inspec version is installed
+describe gem('inspec', :chef) do
+  it { should be_installed }
+  its('version') { should cmp '1.18.0'}
 end
