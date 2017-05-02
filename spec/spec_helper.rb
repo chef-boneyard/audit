8621e8bf1002291479f1c83c7f1483bca342e25d
Christoph Hartmann <chris@lollyrock.com>
2017-05-02 14:57:21 +0200

    add webmock dependency for testing
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 require 'chefspec'
 require 'chefspec/berkshelf'
+require 'webmock/rspec'
 
 RSpec.configure do |config|
   config.file_cache_path = Chef::Config[:file_cache_path]
