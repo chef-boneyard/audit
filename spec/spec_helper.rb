844c04776bc70a499ab1ddf5bdeb35a0e3c8ef2f
Christoph Hartmann <chris@lollyrock.com>
2017-05-02 15:05:38 +0200

    allow internet
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 require 'webmock/rspec'
 
+# Berkshelf needs to connect to internet
+WebMock.allow_net_connect!
+
 RSpec.configure do |config|
   config.file_cache_path = Chef::Config[:file_cache_path]
 end
