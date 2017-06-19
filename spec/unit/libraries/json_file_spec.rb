e88369973e6a8576705811d5037857125d80d870
Christoph Hartmann <chris@lollyrock.com>
2017-06-19 11:00:56 +0200

    ensure json file outputs a json file
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

--- spec/unit/libraries/json_file_spec.rb
+++ spec/unit/libraries/json_file_spec.rb
@@ -8,11 +8,14 @@ require_relative '../../../libraries/reporters/json_file'
 
 describe 'Reporter::JsonFile methods' do
   it 'writes the report to a file on disk' do
-    report = 'some info'
+    report = { 'data' => 'some info' }
     timestamp = Time.now.utc.strftime('%Y%m%d%H%M%S')
     expected_file_path = File.expand_path("inspec-#{timestamp}.json", File.dirname(__FILE__))
     @jsonfile = Reporter::JsonFile.new({file: expected_file_path}).send_report(report)
     expect(File).to exist("#{expected_file_path}")
+    # we sent a ruby hash and expect valid json in the file
+    content = JSON.parse(File.read expected_file_path)
+    expect(content).to eq(report)
     File.delete("#{expected_file_path}")
   end
 end
