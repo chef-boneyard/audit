9cd4df1fd8d835e563a957369986259cdf229951
Adam Leff <adam@leff.co>
2017-08-28 22:25:03 -0400

    Pass Chef node to InSpec as an attribute
    Providing the Chef node object (attributes, environment, etc.) to InSpec
    as an attribute will allow the ability to write more flexible profiles
    and eliminate the need for users to write out JSON files during the
    converge phase only to get read in by the profile later.
    
    Signed-off-by: Adam Leff <adam@leff.co>

--- test/integration/inspec-attributes/default.rb
+++ test/integration/inspec-attributes/default.rb
@@ -11,3 +11,22 @@ describe 'attribute control' do
     expect(attribute_control['status']).to eq('passed')
   end
 end
+
+# Test ability to read in Chef node attributes
+cpu_key_control = controls.find { |x| x['code_desc'] == 'Chef node data - cpu key should exist'}
+cpu_key_control = {} if cpu_key_control.nil?
+
+describe 'cpu_key control' do
+  it 'status should be passed' do
+    expect(cpu_key_control['status']).to eq('passed')
+  end
+end
+
+chef_environment_control = controls.find { |x| x['code_desc'] == 'Chef node data - chef_environment should exist'}
+chef_environment_control = {} if chef_environment_control.nil?
+
+describe 'chef_environment control' do
+  it 'status should be passed' do
+    expect(chef_environment_control['status']).to eq('passed')
+  end
+end
