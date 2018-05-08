830937b7e8ce4d358bd190d8ec3cce943b0fa289
Jared Quick <jquick@chef.io>
2018-05-08 15:12:53 -0400

    Fix tests for chef-client 14.1.1
    Signed-off-by: Jared Quick <jquick@chef.io>

--- test/cookbooks/test_helper/recipes/install_inspec.rb
+++ test/cookbooks/test_helper/recipes/install_inspec.rb
@@ -3,6 +3,10 @@ chef_gem 'inspec-core' do
 end.run_action(:remove)
 
 chef_gem 'inspec' do
+  action :nothing
+end.run_action(:remove)
+
+chef_gem 'inspec' do
   version '1.19.1'
   action :nothing
 end.run_action(:install)
