764962294cd648995d7a909d1f530e9670ed3261
Matt Ray <matthewhray@gmail.com>
2018-08-21 02:04:58 +1000

    Add support for node['audit']['profiles'] as a hash of hashes rather than (#328)
    an array of hashes. Policyfiles with includes do not allow merging arrays
    and it's a bit unclear why an array was chosen. This patch does not
    change the existing behavior, only supplements it.
    
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- files/default/handler/audit_report.rb
+++ files/default/handler/audit_report.rb
@@ -27,7 +27,17 @@ class Chef
         interval = node['audit']['interval']
         interval_enabled = node['audit']['interval']['enabled']
         interval_time = node['audit']['interval']['time']
-        profiles = node['audit']['profiles']
+        if node['audit']['profiles'].class.eql?(Chef::Node::ImmutableMash)
+          profiles = []
+          node['audit']['profiles'].keys.each do |p|
+            h = node['audit']['profiles'][p].to_hash
+            h['name'] = p
+            profiles.push(h)
+          end
+        else
+          Chef::Log.warn "Use of a hash array for the node['audit']['profiles'] is deprecated. Please refer to the README and use a hash of hashes."
+          profiles = node['audit']['profiles']
+        end
         quiet = node['audit']['quiet']
         fetcher = node['audit']['fetcher']
         attributes = node['audit']['attributes'].to_h
