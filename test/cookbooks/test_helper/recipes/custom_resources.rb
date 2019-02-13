f0fb35f9ea47d7edb1fe6b64754c08ba09a5b20a
Will Fisher <wfisher@chef.io>
2019-02-12 16:31:41 -0800

    Make it more clear why there are two of the same profiles being added.
    Basically right now it checks to see if there is already one with the
    same name and will skip.
    
    Signed-off-by: Will Fisher <wfisher@chef.io>

--- test/cookbooks/test_helper/recipes/custom_resources.rb
+++ test/cookbooks/test_helper/recipes/custom_resources.rb
@@ -2,6 +2,7 @@ audit_profile 'ssh-baseline' do
   supermarket 'dev-sec/ssh-baseline'
 end
 
+# this one will be skipped because it was already added by the previous one
 audit_profile 'ssh-baseline' do
-  supermarket 'dev-sec/ssh-baseline'
+  compliance 'admin/ssh-baseline'
 end
