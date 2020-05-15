74289c12b716ee579d1c846a3ca00f942a5da218
Alex Pop <apop@chef.io>
2020-05-15 13:28:11 +0100

    Fixed 5 more style offenses
    Signed-off-by: Alex Pop <apop@chef.io>

--- spec/unit/libraries/cs_automate_spec.rb
+++ spec/unit/libraries/cs_automate_spec.rb
@@ -15,7 +15,7 @@ describe 'Reporter::ChefServerAutomate methods' do
     insecure = false
     @enriched_report_expected = {
       "version": '1.2.1',
-      "profiles":[
+      "profiles": [
         {
           "name": 'tmp_compliance_profile',
           "title": '/tmp Compliance Profile',
@@ -37,7 +37,7 @@ describe 'Reporter::ChefServerAutomate methods' do
               "control 'tmp-1.0' do\n  impact 0.3\n  title 'A /tmp directory must exist'\n  desc 'A /tmp directory must exist'\n  describe file '/tmp' do\n    it { should be_directory }\n  end\nend\n",
               "source_location": { "ref": '/Users/vjeffrey/code/delivery/insights/data_generator/chef-client/cache/cookbooks/test-cookbook/recipes/../files/default/compliance_profiles/tmp_compliance_profile/controls/tmp.rb', "line": 3 },
               "id": 'tmp-1.0',
-              "results": [{ "status": 'passed', "code_desc": 'File /tmp should be directory', "run_time": 0.002312, "start_time": '2016-10-19 11:09:43 -0400' }]
+              "results": [{ "status": 'passed', "code_desc": 'File /tmp should be directory', "run_time": 0.002312, "start_time": '2016-10-19 11:09:43 -0400' }],
             },
             {
               "title": '/tmp directory is owned by the root user',
@@ -56,8 +56,8 @@ describe 'Reporter::ChefServerAutomate methods' do
             },
           ],
           "groups": [{ "title": '/tmp Compliance Profile', "controls": ['tmp-1.0', 'tmp-1.1'], "id": 'controls/tmp.rb' }],
-          "attributes": [{ "name": 'syslog_pkg', "options": { "default": 'rsyslog', "description": 'syslog package...' } }]
-        }
+          "attributes": [{ "name": 'syslog_pkg', "options": { "default": 'rsyslog', "description": 'syslog package...' } }],
+        },
       ],
       "other_checks": [],
       "statistics": { "duration": 0.032332 },
