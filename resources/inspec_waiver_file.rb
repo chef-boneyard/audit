4791795d6d39bb21e7d43e3de18f6d5b83adc06e
Davin Taddeo <davin@chef.io>
2020-09-29 11:15:18 -0400

    some improvements based on the review on the PR I had in the chef-client repo.
    Signed-off-by: Davin Taddeo <davin@chef.io>

--- resources/inspec_waiver_file.rb
+++ resources/inspec_waiver_file.rb
@@ -1,4 +1,6 @@
 require 'yaml'
+require 'date'
+
 provides :inspec_waiver_file
 unified_mode true
 
@@ -16,12 +18,7 @@ property :expiration, String,
   description: 'The expiration date of the given waiver - provided in YYYY-MM-DD format',
   callbacks: {
     'Expiration date should match the following format: YYYY-MM-DD' => proc { |e|
-      re = Regexp.new('([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))').freeze
-      if re.match?(e) || e.nil?
-        true
-      else
-        false
-      end
+      Date.valid_date?(*e.split('-').map(&:to_i))
     },
   }
 
@@ -31,41 +28,44 @@ property :run_test, [true, false],
 property :justification, String,
   description: 'Can be any text you want and might include a reason for the waiver as well as who signed off on the waiver.'
 
-property :backup, [false, Integers],
+property :backup, [false, Integer],
   description: 'The number of backups to be kept in /var/chef/backup (for UNIX- and Linux-based platforms) or C:/chef/backup (for the Microsoft Windows platform). Set to false to prevent backups from being kept.',
   default: false
 
 action :add do
   filename = new_resource.file
-  yaml_contents = if ::File.file?(filename) && ::File.readable?(filename) && !::File.zero?(filename)
-                    IO.read(new_resource.file)
-                  else
-                    ''
-                  end
-  waiver_hash = {}
-  waiver_hash = ::YAML.safe_load(yaml_contents) unless yaml_contents.empty?
+  waiver_hash = load_waiver_file_to_hash(filename)
   control_hash = {}
   control_hash['expiration_date'] = new_resource.expiration.to_s unless new_resource.expiration.nil?
   control_hash['run'] = new_resource.run_test unless new_resource.run_test.nil?
   control_hash['justification'] = new_resource.justification.to_s
 
-  if waiver_hash.key?("#{new_resource.control}")
-    unless waiver_hash["#{new_resource.control}"] == control_hash
-      waiver_hash["#{new_resource.control}"] = {}
-      waiver_hash["#{new_resource.control}"] = control_hash
-      waiver_hash = waiver_hash.sort.to_h
-      file "Update Waiver File #{new_resource.file} to update waiver for control #{new_resource.control}" do
-        path new_resource.file
-        content waiver_hash.to_yaml
-        backup new_resource.backup
-        action :create
-      end
+  if waiver_hash.key?(new_resource.control)
+    unless waiver_hash[new_resource.control] == control_hash
+      waiver_hash.delete(new_resource.control)
+      waiver_hash[new_resource.control] = control_hash
     end
   else
-    waiver_hash["#{new_resource.control}"] = {}
-    waiver_hash["#{new_resource.control}"] = control_hash
+    waiver_hash[new_resource.control] = control_hash
+  end
+
+  waiver_hash = waiver_hash.sort.to_h
+
+  file "Update Waiver File #{new_resource.file} to update waiver for control #{new_resource.control}" do
+    path new_resource.file
+    content waiver_hash.to_yaml
+    backup new_resource.backup
+    action :create
+  end
+end
+
+action :remove do
+  filename = new_resource.file
+  waiver_hash = load_waiver_file_to_hash(filename)
+  if waiver_hash.key?(new_resource.control)
+    waiver_hash.delete(new_resource.control)
     waiver_hash = waiver_hash.sort.to_h
-    file "Update Waiver File #{new_resource.file} to add waiver for control #{new_resource.control}" do
+    file "Update Waiver File #{new_resource.file} to remove waiver for control #{new_resource.control}" do
       path new_resource.file
       content waiver_hash.to_yaml
       backup new_resource.backup
@@ -74,20 +74,15 @@ action :add do
   end
 end
 
-action :remove do
-  filename = new_resource.file
-  if ::File.file?(filename) && ::File.readable?(filename) && !::File.zero?(filename)
-    yaml_contents = IO.read(filename)
-    waiver_hash = ::YAML.safe_load(yaml_contents)
-    if waiver_hash.key?("#{new_resource.control}")
-      waiver_hash.delete("#{new_resource.control}")
-      waiver_hash = waiver_hash.sort.to_h
-      file "Update Waiver File #{new_resource.file} to remove waiver for control #{new_resource.control}" do
-        path new_resource.file
-        content waiver_hash.to_yaml
-        backup new_resource.backup
-        action :create
-      end
+action_class do
+  def load_waiver_file_to_hash(file_name)
+    if ::File.file?(file_name) && ::File.readable?(file_name) && !::File.zero?(file_name)
+      file_contents = IO.read(file_name)
+      contents_hash = {}
+      contents_hash = ::YAML.safe_load(file_contents) unless file_contents.empty?
+    else
+      contents_hash = {}
     end
+    contents_hash
   end
 end
