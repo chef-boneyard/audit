5e3fe8edbaf1c8394c6e7b1540109697f0a608f4
Milan Stastny <milan@stastnej.ch>
2021-01-26 19:16:07 +0100

    Fix Offenses

--- libraries/reporters/cli.rb
+++ libraries/reporters/cli.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
 require 'chef/handler'
 
@@ -7,58 +7,65 @@ module Reporter
   # Used to raise an error on conformance failure
   #
   class Cli
-
     def send_report(report)
       # iterate over each profile and control
       output = []
       report[:profiles].each do |profile|
         next if profile[:controls].nil?
+
         output << "\n"
         output << profile[:title]
         profile[:controls].each do |control|
           next if control[:results].nil?
+
           output << "\t#{control[:title]}"
           control[:results].each do |result|
-            if result[:status] == 'failed'
-              found = false
-              if result[:code_desc]
-                found = true
-                output << "\t\t\033[31m\xE2\x9D\x8C #{result[:code_desc]}\033[0m"
-              end
-              if result[:message]
-                if found
-                  result[:message].split(/\n/).each do |m|
-                    next if m.empty?
-                    output << "\t\t\t\033[31m#{m}\033[0m"
-                  end
-                else
-                  prefix = "\xE2\x9D\x8C"
-                  result[:message].split(/\n/).each do |m|
-                    next if m.empty?
-                    output << "\t\t\033[31m#{prefix}#{m}\033[0m"
-                    prefix = ""
-                  end
-                end
-                found = true
-              end
-              unless found
-                output << "\t\t\033[31m\xE2\x9D\x8C #{result[:status]}\033[0m"
-              end
-            else
-              found = false
-              if result[:code_desc]
-                found = true
-                output << "\t\t\033[32m\xE2\x9C\x94 #{result[:code_desc]}\033[0m"
-              end
-              unless found
-                output << "\t\t\033[32m\xE2\x9C\x94 #{result[:status]}\033[0m"
-              end
-            end
+            format_result(result)
           end
         end
       end
       output << "\n"
       puts output.join("\n")
+    end
+
+    private
+
+    def format_result(result)
+      output = []
+      found = false
+      if result[:status] == 'failed'
+        if result[:code_desc]
+          found = true
+          output << "\t\t\033[31m\xE2\x9D\x8C #{result[:code_desc]}\033[0m"
+        end
+        if result[:message]
+          if found
+            result[:message].split(/\n/).reject(&:empty?).each do |m|
+              output << "\t\t\t\033[31m#{m}\033[0m"
+            end
+          else
+            prefix = "\xE2\x9D\x8C"
+            result[:message].split(/\n/).reject(&:empty?).each do |m|
+              output << "\t\t\033[31m#{prefix}#{m}\033[0m"
+              prefix = ''
+            end
+          end
+          found = true
+        end
+        unless found
+          output << "\t\t\033[31m\xE2\x9D\x8C #{result[:status]}\033[0m"
+        end
+      else
+        found = false
+        if result[:code_desc]
+          found = true
+          output << "\t\t\033[32m\xE2\x9C\x94 #{result[:code_desc]}\033[0m"
+        end
+        unless found
+          output << "\t\t\033[32m\xE2\x9C\x94 #{result[:status]}\033[0m"
+        end
+      end
+      output
     end
   end
 end
